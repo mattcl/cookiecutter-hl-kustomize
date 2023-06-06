a664384bc4ef7ad4b3f9005acb6894b80c0a3797
Matt Chun-Lum <matt@questionable.engineering>
2023-06-05 19:30:14 -0700

    feat: add support for generating db component.
    For now this is limited to postgres

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,52 @@
 import os
+import subprocess
+
+# if we're using a db, we're going to generate the sealed secret and remove the
+# file that created the secret before potentially committing to the git repo
+generate_db_secret = {% if cookiecutter.database != "none" %}True{% else %}False{% endif %}
+
+if generate_db_secret:
+    import base64
+
+    print("> Generating DB passwords")
+    # we want to replace the placeholders in the generated file with the encoded
+    # generated secrets. We're just going to use the cookiecutter extension here
+    # to generate the random strings
+    db_password = base64.b64encode("{{ random_ascii_string(24, punctuation=False) }}".encode("utf-8"))
+    db_postgres_password = base64.b64encode("{{ random_ascii_string(24, punctuation=False) }}".encode("utf-8"))
+
+    with open("installs/default/raw-db-secret.yaml", "r") as file:
+        contents = file.read()
+
+    contents = contents.replace("REPLACE_POSTGRES_PASSWORD", str(db_password, "utf-8"))
+    contents = contents.replace("REPLACE_POSTGRES_POSTGRES_PASSWORD", str(db_postgres_password, "utf-8"))
+
+    # write the modified content back to the file
+    with open("installs/default/raw-db-secret.yaml", "w") as file:
+        file.write(contents)
+
+    print("> Sealing generated secret")
+
+    subprocess.call([
+        "kubeseal",
+        "--format",
+        "yaml",
+        "--scope",
+        "namespace-wide",
+        "-n",
+        "{{ cookiecutter.namespace }}",
+        "-f",
+        "installs/default/raw-db-secret.yaml",
+        "-w",
+        "installs/default/db-secret.yaml"
+    ])
 
 # remove unneeded files
 REMOVE_PATHS = [
     '{% if cookiecutter.ingress == "none" %}components/app/ingress.yaml{% endif %}',
+    '{% if cookiecutter.database == "none" %}components/db{% endif %}',
+    # we always remove this regardless
+    'installs/default/raw-db-secret.yaml',
 ]
 
 for path in REMOVE_PATHS:
@@ -13,6 +57,7 @@ for path in REMOVE_PATHS:
         else:
             os.unlink(path)
 
+
 # Initialize git repository if requested
 # the yes/no bool flags in the json are not in the stable release of
 # cookiecutter that's on pypi
@@ -20,7 +65,6 @@ init_git = {% if cookiecutter.initialize_git_repo == "yes" %}True{% else %}False
 
 if init_git:
     print("> Initializing git repository")
-    import subprocess
     subprocess.call(["git", "init"])
     subprocess.call(["git", "add", "*"])
     subprocess.call(["git", "commit", "-m", "Cookiectutter generated initial commit"])
